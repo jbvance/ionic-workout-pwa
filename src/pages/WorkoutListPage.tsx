c800bdf69349f8a95d533f5b635590e9b031d211
jbvance <jbvance@gmail.com>
2019-11-22 12:50:55 -0500

    add link to workout images in WorkoutListPage

--- src/pages/WorkoutListPage.tsx
+++ src/pages/WorkoutListPage.tsx
@@ -1,20 +1,49 @@
 import { IonButtons, IonContent, IonHeader, IonIcon, IonItem, IonList, IonMenuButton, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import { americanFootball, basketball, beer, bluetooth, boat, build, flask, football, paperPlane, wifi } from 'ionicons/icons';
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 import WorkoutImage from '../components/WorkoutImage';
 
 type Item = {
-src: string;
-text: string;
+  src: string;
+  text: string;
+  url: '/'
 };
 
 const workoutItems: Item[] = [
-  { src: 'https://imgix.bustle.com/elite-daily/2016/03/06140813/elite-daily-Suprijono-Suharjoto-workout.jpg?w=1020&h=574&fit=crop&crop=faces&auto=format&q=70', text: 'Back Strength' },
-  { src: 'https://www.cheatsheet.com/wp-content/uploads/2016/09/Cute-fit-girl-doing-physical-exercises-at-gym.jpg', text: 'Core Workout'},
-  {src: 'https://myfitnessgear.co.uk/wp-content/uploads/2018/07/cant-feel-my-abs-working-when-doing-sit-ups-5.png', text: 'All Abs'}
+  { 
+    src: 'https://imgix.bustle.com/elite-daily/2016/03/06140813/elite-daily-Suprijono-Suharjoto-workout.jpg?w=1020&h=574&fit=crop&crop=faces&auto=format&q=70', 
+    text: 'Back Strength',
+    url: '/'
+  },
+  { 
+    src: 'https://www.cheatsheet.com/wp-content/uploads/2016/09/Cute-fit-girl-doing-physical-exercises-at-gym.jpg', 
+    text: 'Core Workout',
+    url: '/'
+  },
+  {
+    src: 'https://myfitnessgear.co.uk/wp-content/uploads/2018/07/cant-feel-my-abs-working-when-doing-sit-ups-5.png', 
+    text: 'All Abs',
+    url: '/'
+  }
 
 ];
+
+const ListItems = () => {
+ 
+
+  const items = workoutItems.map(item => {
+    return (
+      <IonItem key={item.text}>
+       <Link to='/'>
+          <WorkoutImage src={item.src} text={item.text} />
+        </Link>      
+      </IonItem>
+    );
+  });
+
+  return <IonList>{items}</IonList>;
+};
 
 const WorkoutListPage: React.FC = () => {
   return (
@@ -29,34 +58,12 @@ const WorkoutListPage: React.FC = () => {
       </IonHeader>
 
       <IonContent>
-        <ListItems />
+       <ListItems />
       </IonContent>
     </IonPage>
   );
 };
 
-const ListItems = () => {
-  const icons = [
-    flask,
-    wifi,
-    beer,
-    football,
-    basketball,
-    paperPlane,
-    americanFootball,
-    boat,
-    bluetooth,
-    build
-  ];
-
-  const items = workoutItems.map(item => {
-    return (
-      <WorkoutImage key={item.text} src={item.src} text={item.text} />
-    );
-  });
-
-  return <IonList>{items}</IonList>;
-};
 
 export default WorkoutListPage;
 
